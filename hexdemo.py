03c3d86afb48d36705639f963bd070dcf070489c
Brandon Newbright <newbright.2@osu.edu>
2015-06-11 17:26:04 -0400

    Initial commit.

---
+++ hexdemo.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------- 
+# Author: Brandon Newbright
+# 
+# Description: Basic Python implementation of 
+# 	hexagonal grid system, for game development.
+#--------------------------------------------------
+
+import collections
+import math
+
+# Class to store and construct "cube" coordinates
+class Hex(object):
+
+	Point = collections.namedtuple("Point", [])
+
+	def __init__(self, q, r, s)
+		self = 
