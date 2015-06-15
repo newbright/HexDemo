3356235e27c3fd438befde2dc6459f0f52e48354
Brandon Newbright <newbright.2@osu.edu>
2015-06-15 16:03:42 -0400

    Add linear interpolation and draw methods.

--- hexdemo.py
+++ hexdemo.py
@@ -22,13 +22,6 @@ class Hex(object):
 	def __ne__(self, other):
 		return !(self == other)
 
-# Minimal class to store two-dimensional point coordinates
-class Point(object):
-
-	# Stores a pair of Cartesian coordinates. Pretty straighforward.
-	def __init__(self, x, y):
-		self.x, self.y = x, y
-
 # Class to house mathematic functions on and between Hex() objects
 class HexOps:
 
@@ -62,6 +55,36 @@ class HexOps:
 	def neighbor(self, a, d):
 		return add(a, direction(d))
 
+	# Rounds the coordinates of a "fractional" Hex() 'a' into integers
+	def hex_round(self, a):
+		q = int(round(a.q))
+		r = int(round(a.r))
+		s = int(round(a.s))
+		q_diff = abs(q - a.q)
+		r_diff = abs(r - a.r)
+		s_diff = abs(s - a.s)
+		if q_diff > r_diff and q_diff > s_diff:
+			q = -r - s
+		else:
+			if r_diff > s_diff:
+				r = -q - s
+			else:
+				s = -q - r
+		return Hex(q, r, s)
+
+	# Linearly interpolates between two hexagons 'a' and 'b', where 't' is the the geometric sum (1.0/N) * i for 0 < i < N
+	def hex_lerp(self, a, b, t):
+		return Hex(a.q + (b.q - a.q) * t, a.r + (b.r - a.r) * t, a.s + (b.s - a.s) * t)
+
+	# Uses linear interpolation to draw a line between the hexagons 'a' and 'b'
+	def hex_linedraw(self, a, b):
+		N = hex_distance(a, b)
+		results = []
+		step = 1.0 / max(N, 1)
+		for i in range(0, N + 1):
+			results.append(hex_round(hex_lerp(a, b, step * i)))
+		return results
+
 # Helper class to define the hexagonal grid's orientation
 class Orientation(object):
 
@@ -78,6 +101,13 @@ class Orientation(object):
 		if type_name == "flat":
 			return Orientation(3.0 / 2.0, 0.0, sqrt(3.0) / 2.0, sqrt(3.0), 2.0 / 3.0, 0.0, -1.0 / 3.0, sqrt(3.0) / 3.0, 0.0)
 
+# Minimal class to store two-dimensional point coordinates
+class Point(object):
+
+	# Stores a pair of Cartesian coordinates. Pretty straighforward.
+	def __init__(self, x, y):
+		self.x, self.y = x, y
+
 # Class to establish the layout of the hexagonal grid on-screen
 class Layout(object):
 
@@ -86,3 +116,30 @@ class Layout(object):
 		self.orientation = orientation
 		self.size = size
 		self.origin = origin
+
+	# Finds the pixel representation of a given hexagon
+	def hex_to_pixel(self, hexagon):
+		x = (self.orientation.f0 * hexagon.q + self.orientation.f1 * hexagon.r) * self.size.x
+		y = (self.orientation.f2 * hexagon.q + self.orientation.f3 * hexagon.r) * self.size.y
+		return Point(x + self.origin.x, y + self.origin.y)
+
+	# Finds the hexagon at a given pixel
+	def pixel_to_hex(self, point):
+		pt = Point((point.x - self.origin.x) / self.size.x, (point.y - self.origin.y) / self.size.y)
+		q = self.orientation.b0 * pt.x + self.orientation.b1 * pt.y
+		r = self.orientation.b2 * pt.x + self.orientation.b3 * pt.y
+		return Hex(q, r, -q - r)
+
+	# Finds the vector location of a hexagon's corner at "corner" degrees, relative to the hexagon's center
+	def hex_corner_offset(self, corner):
+		angle = 2.0 * math.pi * (corner + self.orientation.l) / 6
+		return Point(self.size.x * cos(angle), self.size.y * sin(angle))
+
+	# Finds the vector locations of all six corners of a hexagon
+	def polygon_corners(self, hexagon):
+		corners = []
+		center = self.hex_to_pixel(hexagon)
+		for i in range(0, 6):
+			offset = hex_corner_offset(i)
+			corners.append(Point(center.x + offset.x, center.y + offset.y))
+		return corners
